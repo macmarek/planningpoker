ce80e595276b71945b64c73d1c9faa75803dcd1a
Marek Czarnecki <macmarek@gmail.com>
2016-09-09 13:25:17 +0200

    added material design and small ui improvements

--- PlanningPoker/App_Start/BundleConfig.cs
+++ PlanningPoker/App_Start/BundleConfig.cs
@@ -23,9 +23,9 @@ namespace PlanningPoker
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/session").Include(
                       "~/Scripts/app/sessionByShortId.js"));
